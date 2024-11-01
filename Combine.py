25204accb1e720084984db6b6e90165cd89eedcc
1rone <57466060+1rone@users.noreply.github.com>
2024-11-01 18:55:20 +0200

    Update (#92)
    * Create Gitberry
    * Update and rename Pubcircle.txt to Axis.txt
    * Update Adinify, Chartboost, DTExchange, Gadsme, LiftoffMonetize, Mintegral, UnityAds
    
    ---------
    
    Co-authored-by: Str4tos <eschenkoden@gmail.com>
    Co-authored-by: Dmytro Uzhva <kraativ@gmail.com>

--- Combine.py
+++ Combine.py
@@ -14,7 +14,7 @@ _ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 _NETS_DIR_NAME = "Networks"
 _DSP_DIR_NAME = "InternalExchange"
 
-_SOURCES = [ 
+_SOURCES = [
     "GoogleAds",
     "AudienceNetwork",
     "Pangle",
@@ -26,7 +26,7 @@ _SOURCES = [
     "SuperAwesome",
     "Kidoz",
     "InMobi",
-    "Chartboost", 
+    "Chartboost",
     "YandexAds",
     "DTExchange",
     "Bigo",
@@ -55,11 +55,12 @@ _SOURCES_CAS = [
     "SmartyAds",
     "GothamAds",
     "RTBHouse",
-    "Pubcircle",
+    "Axis",
     "TheGermaneMedia",
     "Bidscube",
     "Kueez",
-    "Tappx"
+    "Tappx",
+    "Gitberry",
 ]
 _SOURCE_DSP = [
     "A4G",
@@ -74,14 +75,14 @@ _SOURCE_IN_GAMES = [
 ]
 _BANS = [
     # (Reserved by Network name, Banned domain for other Networks)
-    #("AdMob", "google.com")
+    # ("AdMob", "google.com")
 ]
-_VARIABLES = { # SUPPORTED VARIABLES
-    #"contact", # contact information
-    #"subdomain", # pointer to a subdomain file
-    #"inventorypartnerdomain", # reference is followed to an ads.txt file only (not app-ads.txt)
-    #"ownerdomain", # specifies the business domain of the business entity that owns the domain/site/app
-    #"managerdomain", # Specifies the business domain of a primary or exclusive monetization partner of the publishers inventory
+_VARIABLES = {  # SUPPORTED VARIABLES
+    # "contact", # contact information
+    # "subdomain", # pointer to a subdomain file
+    # "inventorypartnerdomain", # reference is followed to an ads.txt file only (not app-ads.txt)
+    # "ownerdomain", # specifies the business domain of the business entity that owns the domain/site/app
+    # "managerdomain", # Specifies the business domain of a primary or exclusive monetization partner of the publishers inventory
 }
 _DOMAIN_PATTERN = re.compile(r"^([a-z0-9-]{1,63}\.)+[a-z]{2,9}\Z")
 _ID_PATTERN = re.compile("^[a-zA-Z0-9-_]+$")
@@ -90,37 +91,52 @@ _CERTIFICATE_PATTERN = re.compile("^[a-zA-Z0-9]+$")
 certificateMap = dict()
 
 arg_parser = argparse.ArgumentParser(
-        prog='python Combine.py',
-        description=(
-            'This script can update App-ads.txt for each Ad Networks and combine all to main file.'),
-        epilog='Powered by CAS.AI')
+    prog='python Combine.py',
+    description=(
+        'This script can update App-ads.txt for each Ad Networks and combine all to main file.'),
+    epilog='Powered by CAS.AI')
 
 arg_subparsers = arg_parser.add_subparsers()
 
-arg_init = arg_subparsers.add_parser('init', help='Create ' + _TEMP_FILE + ' file to update network configuration.')
+arg_init = arg_subparsers.add_parser(
+    'init', help='Create ' + _TEMP_FILE + ' file to update network configuration.')
 arg_init.add_argument('file', action='store_true')
-arg_init.add_argument('-l', '--list', action='store_true', help='List of available network names.')
+arg_init.add_argument('-l', '--list', action='store_true',
+                      help='List of available network names.')
 arg_init.set_defaults(network=None, release=False, unique_id=False)
 
-arg_update = arg_subparsers.add_parser('update', help='Check each inventory in ' + _TEMP_FILE + ' with inventories in network file.')
-arg_update.add_argument('network', help='The file name with network inventories from `' + _NETS_DIR_NAME + '` directory.')
-arg_update.add_argument('-f', '--force', action='store_true', help='Replacing all inventories in the network file.')
-arg_update.add_argument('-r', '--release', action='store_true', help='Final ' + _RESULT_FILE + ' file generation.')
-arg_update.add_argument('--unique-id', action='store_true', help='Verification of unique certification identifiers for each domain.')
-arg_update.add_argument('--no-fill-id', dest='fillCertificate', action='store_false', help='Disable autocomplete of known certification identifiers for each domain.')
+arg_update = arg_subparsers.add_parser(
+    'update', help='Check each inventory in ' + _TEMP_FILE + ' with inventories in network file.')
+arg_update.add_argument(
+    'network', help='The file name with network inventories from `' + _NETS_DIR_NAME + '` directory.')
+arg_update.add_argument('-f', '--force', action='store_true',
+                        help='Replacing all inventories in the network file.')
+arg_update.add_argument('-r', '--release', action='store_true',
+                        help='Final ' + _RESULT_FILE + ' file generation.')
+arg_update.add_argument('--unique-id', action='store_true',
+                        help='Verification of unique certification identifiers for each domain.')
+arg_update.add_argument('--no-fill-id', dest='fillCertificate', action='store_false',
+                        help='Disable autocomplete of known certification identifiers for each domain.')
 arg_update.set_defaults(file=False, release=False)
 
-arg_release = arg_subparsers.add_parser('release', help='Final ' + _RESULT_FILE + ' file generation.')
-arg_release.add_argument('-g', '--for-games', dest='games', action='store_true', help='Release App-ads-games.txt for Games.')
-arg_release.set_defaults(release=True, file=False, network=None, unique_id=False, fillCertificate=True)
+arg_release = arg_subparsers.add_parser(
+    'release', help='Final ' + _RESULT_FILE + ' file generation.')
+arg_release.add_argument('-g', '--for-games', dest='games',
+                         action='store_true', help='Release App-ads-games.txt for Games.')
+arg_release.set_defaults(release=True, file=False,
+                         network=None, unique_id=False, fillCertificate=True)
 
 args = arg_parser.parse_args()
 
+
 def print_warning(warning, inventory):
-    print('\033[93m   Warning: ' + warning + '\n      ' + inventory + '\033[0m')
+    print('\033[93m   Warning: ' + warning +
+          '\n      ' + inventory + '\033[0m')
+
 
 def fatal_error(error, inventory=''):
     sys.exit('\033[91m   Error: ' + error + '\n      ' + inventory + '\033[0m')
+
 
 @total_ordering
 class Inventory:
@@ -143,24 +159,27 @@ class Inventory:
             if pattern[0] not in _VARIABLES:
                 fatal_error("Not supported variable in " + source + ".", line)
             if not re.match(_DOMAIN_PATTERN, pattern[1]):
-                fatal_error("Invalid domain '" + pattern[1] + "' for variable in " + source, line)
+                fatal_error("Invalid domain '" +
+                            pattern[1] + "' for variable in " + source, line)
             return
         pattern = line.split(',')
         if len(pattern) != 3 and len(pattern) != 4:
-            fatal_error("Invalid pattern in " + source + ". It may only contain 3 or 4 segments.", line)
+            fatal_error("Invalid pattern in " + source +
+                        ". It may only contain 3 or 4 segments.", line)
 
         self.domain = pattern[0].strip().lower()
         if not re.match(_DOMAIN_PATTERN, self.domain):
             fatal_error("Invalid domain in " + source, line)
-        
+
         for banDomain in _BANS:
             if source != banDomain[0] and self.domain == banDomain[1]:
                 self.domain = None
                 return
-            
+
         self.type = pattern[2].split('#')[0].strip().upper()
         if self.type != 'RESELLER' and self.type != 'DIRECT':
-            fatal_error("Invalid pattern in " + source + ". Must be RESELLER or DIRECT only.", line)
+            fatal_error("Invalid pattern in " + source +
+                        ". Must be RESELLER or DIRECT only.", line)
 
         self.identifier = pattern[1].strip()
         if not re.match(_ID_PATTERN, self.identifier):
@@ -172,34 +191,39 @@ class Inventory:
                 self.certification = certification
                 if (len(certification) != 9 and len(certification) != 16) or not re.match(_CERTIFICATE_PATTERN, self.certification):
                     if self.domain in certificateMap:
-                        fatal_error("Certification authority ID for " + self.domain + " is " + certificateMap[self.domain], line)
-                    else:    
-                        fatal_error("Certification authority ID is invalid in " + source + ".\nIt may only contain numbers and lowercase letters, and must be 9 or 16 characters.", line)
+                        fatal_error("Certification authority ID for " +
+                                    self.domain + " is " + certificateMap[self.domain], line)
+                    else:
+                        fatal_error("Certification authority ID is invalid in " + source +
+                                    ".\nIt may only contain numbers and lowercase letters, and must be 9 or 16 characters.", line)
                 elif self.domain in certificateMap:
                     if not certificateMap[self.domain]:
-                        print_warning("Certification authority ID is should be empty for " + self.domain + " in " + source, line)
+                        print_warning(
+                            "Certification authority ID is should be empty for " + self.domain + " in " + source, line)
                         self.certification = ""
                     elif certificateMap[self.domain] != certification:
-                        print_warning("Certification authority ID not mach with " + certificateMap[self.domain] + " in " + source, line)
+                        print_warning("Certification authority ID not mach with " +
+                                      certificateMap[self.domain] + " in " + source, line)
                 elif args.unique_id:
                     try:
                         readyDomain = certificateMap.values().index(certification)
-                        print_warning("Certification authority ID is already taken by " + 
-                                (certificateMap.keys()[readyDomain]) + " domain. In " + source, line)
+                        print_warning("Certification authority ID is already taken by " +
+                                      (certificateMap.keys()[readyDomain]) + " domain. In " + source, line)
                     except ValueError:
                         certificateMap[self.domain] = certification
                 else:
-                    print_warning("Add unknown certification: " + certification + " for " + self.domain, line)
+                    print_warning("Add unknown certification: " +
+                                  certification + " for " + self.domain, line)
                     certificateMap[self.domain] = certification
 
     def __eq__(self, other):
         if (isinstance(other, Inventory)
-            and self.domain == other.domain 
+            and self.domain == other.domain
             and self.identifier == other.identifier
             and self.comment == other.comment
-            and self.variable == other.variable):
+                and self.variable == other.variable):
             if self.type != other.type:
-                print_warning("Relationship is already set " + self.type + " by " + self.source + 
+                print_warning("Relationship is already set " + self.type + " by " + self.source +
                               "\nPlease fix conflict with " + other.source, other.to_line())
             return True
         return False
@@ -221,13 +245,13 @@ class Inventory:
         if not self.domain:
             return hash("")
         return hash(hash(self.domain) + hash(self.identifier))
-    
+
     def is_comment(self):
         return self.comment
-    
+
     def is_empty(self):
         return not self.domain and not self.comment and not self.variable
-    
+
     def to_line(self, fillCertificate=False):
         if self.comment:
             return self.comment
@@ -240,15 +264,18 @@ class Inventory:
             result += ', ' + certificateMap[self.domain]
         return result + '\n'
 
+
 def read_certifications():
     path = os.path.join(_ROOT_DIR, _CERTIFICATIONS_FILE)
     if os.path.exists(path):
         with open(path, "r") as file:
             certificateMap.update(json.load(file))
 
+
 def save_certifications():
     with open(os.path.join(_ROOT_DIR, _CERTIFICATIONS_FILE), "w") as file:
         json.dump(certificateMap, file, indent=2, sort_keys=True)
+
 
 def release():
     currentDate = date.today().strftime("%b %d, %Y")
@@ -261,7 +288,7 @@ def release():
         mainFilePath = os.path.join(_ROOT_DIR, _RESULT_FOR_GAMES_FILE)
     else:
         mainFilePath = os.path.join(_ROOT_DIR, _RESULT_FILE)
-        
+
     if os.path.exists(mainFilePath):
         with open(mainFilePath, "r") as appAdsFile:
             totalLines = str(sum(1 for _ in appAdsFile) - 1)
@@ -296,7 +323,9 @@ def release():
     with open(os.path.join(_ROOT_DIR, "Shield.json"), "w") as shiledFile:
         json.dump(shiledInfo, shiledFile)
 
-    print("Combined " + _RESULT_FILE + " with " + str(len(inventorySet)) + " (was " + totalLines + ") inventories for " + str(len(_SOURCES)) + " networks.")
+    print("Combined " + _RESULT_FILE + " with " + str(len(inventorySet)) +
+          " (was " + totalLines + ") inventories for " + str(len(_SOURCES)) + " networks.")
+
 
 def update_dsp(networkName, sourceNames):
     newInventories = set()
@@ -308,7 +337,8 @@ def update_dsp(networkName, sourceNames):
                     continue
                 newInventories.add(inventory)
     return update_items(networkName, newInventories, force=False, keepHead=False)
-        
+
+
 def update(networkName, force):
     newInventories = set()
     with open(os.path.join(_ROOT_DIR, _TEMP_FILE), 'r') as updateFile:
@@ -318,6 +348,7 @@ def update(networkName, force):
                 continue
             newInventories.add(inventory)
     return update_items(networkName, newInventories, force, keepHead=True)
+
 
 def update_items(networkName, newInventories, force, keepHead):
     duplicate = 0
@@ -340,7 +371,8 @@ def update_items(networkName, newInventories, force, keepHead):
                 continue
             if inventory in inventorySet:
                 duplicate += 1
-                print_warning("Duplicate in " + networkName, inventory.to_line())
+                print_warning("Duplicate in " + networkName,
+                              inventory.to_line())
                 continue
             if keepHead:
                 if not keepInventories or keepInventories[0].domain == inventory.domain:
@@ -352,7 +384,7 @@ def update_items(networkName, newInventories, force, keepHead):
     if not force and len(diffInventories) == 0 and duplicate == 0:
         print("No found inventories to update for " + networkName)
         return False
-    
+
     print("Update " + networkName + " inventories")
     for inventory in keepInventories:
         sys.stdout.write("[Keep] " + inventory.to_line())
@@ -367,7 +399,7 @@ def update_items(networkName, newInventories, force, keepHead):
         userSelect = raw_input(inputMessage)
     else:
         userSelect = input(inputMessage)
-        
+
     if userSelect.lower() == 'f':
         force = True
     else:
@@ -375,19 +407,22 @@ def update_items(networkName, newInventories, force, keepHead):
 
     if force or userSelect.lower() == 'y':
         with open(os.path.join(_ROOT_DIR, resultDir, networkName + ".txt"), 'w') as sourceFile:
-            sourceFile.write("#=== " + networkName + " " + date.today().strftime("%b %d, %Y") + '\n')
+            sourceFile.write("#=== " + networkName + " " +
+                             date.today().strftime("%b %d, %Y") + '\n')
             for inventory in sorted(keepInventories):
                 sourceFile.write(inventory.to_line())
                 newInventories.discard(inventory)
 
-            #result = list(newInventories)
-            #result.sort()
+            # result = list(newInventories)
+            # result.sort()
             for inventory in sorted(newInventories):
                 sourceFile.write(inventory.to_line(fillCertificate))
 
-        print("Updated " + networkName + " with " + str(len(newInventories) + len(keepInventories)) + " inventories.")
+        print("Updated " + networkName + " with " +
+              str(len(newInventories) + len(keepInventories)) + " inventories.")
         return True
     return False
+
 
 if args.file == True:
     open(os.path.join(_ROOT_DIR, _TEMP_FILE), 'w+').close()
